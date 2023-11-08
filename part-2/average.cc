eb7abb298380d740edbf5572eda4b54de9a25002
Audrey Thomas <audrey.celeste.thomas@gmail.com>
2023-11-08 19:59:39 +0000

    Final commit

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,7 +1,7 @@
-// Dillon Garcia
-// dgarcia0562@csu.fullerton.edu
-// dgarcia0562
-// partner: audreythomas
+// Audrey Thomas
+// audrey_thomas101@csu.fullerton.edu
+// @audreythomas
+// Partners: @audreythomas, @dgarcia05
 
 #include <iostream>
 #include <string>
@@ -10,68 +10,22 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-   if (arguments.size() == 4){ 
-     std::cout << "error: you must supply four arguments"; 
-     return 1; 
-   } 
+  if ((arguments.size() - 1) < 1) {
+    std::cout << "error: you must supply at least one number";
+    return 1;
+  }
+  double addup = 0.0;
+  bool firstindex = true;
 
-   std::string street{arguments[1]}; 
-
-   std::string day{arguments[2]}; 
-
-   int hour{}; 
-   hour = std::stoi(arguments[3]); 
-
-   int minute{}; 
-   minute = std::stoi(arguments[4]);
-
-   if(street != "ash" && street != "beech" && street != "cedar" && street != "date" && street != "elm") { 
-     std::cout << "error: invalid street"; 
-     return 1; 
-   } 
-
-   if (day != "sun" && day != "mon" && day != "tue" && day != "wed" && day != "thu" && day != "fri" && day != "sat") { 
-     std::cout << "error: invalid day"; 
-     return 1; 
-   
-   }
-
-  if (hour < 1 (hour) > 24) { 
-     std::cout << "error: invalid hour"; 
-     return 1; 
-
-   } 
-
-  if (minute < 0 (minute) > 59) { 
-     std::cout << "error: invalid minute"; 
-     return 1; 
-
-   } 
-
-    bool parking{}; 
-
-   if (street == "ash"){ 
-   parking = ParkOnAsh(day, hour); 
-
-   } else if (street == "beech"){ 
-     parking = ParkOnBeech(day, hour); 
-
-   } else if (street == "Cedar"){ 
-     parking = ParkOnCedar(day, hour); 
-
-   } else if (street == "date"){ 
-     parking = ParkOnDate(day, hour, minute); 
-
-   } else if (street == "elm"){ 
-     parking = ParkonElm(day,hour, minute) 
-   }
-
-   if (parking == true){ 
-     std::cout << "allowed"; 
-
-   }else{ 
-     std::cout << "not allowed"; 
-   } 
-
+  for (const std::string& value : arguments) {
+    if (firstindex) {
+      firstindex = false;
+      continue;
+    }
+    addup += std::stod(value);
+  }
+  int denominator = static_cast<int>(arguments.size()) - 1;
+  double result = addup / denominator;
+  std::cout << "average = " << result;
   return 0;
 }
